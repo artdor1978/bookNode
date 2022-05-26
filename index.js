22495f756987b5b0ca754f0949a7bbfca9104d0f
artdor1978 <artdor1978@gmail.com>
2022-05-26 20:53:50 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+app.use(express.static('public'))
+app.listen(3000, () => {
+    console.log("App listening on port 3000")
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+})
+app.get('/about', (req, res) => { // called when request to /about comes in 
+    res.sendFile(path.resolve(__dirname, 'about.html'))
+})
+app.get('/contact', (req, res) => { //called when request to /contact comes 
+    res.sendFile(path.resolve(__dirname, 'contact.html'))
+})
